8e3467b192e8e1a869f3e701e507c5696642fd79
qurias <qurias_it@mail.ru>
2023-05-20 21:02:58 +0300

    change link tag a to nuxt-link in header and change title in nuxt.config.js

--- components/Main/Header.vue
+++ components/Main/Header.vue
@@ -1,7 +1,11 @@
 <template>
     <header id="masthead" class="site-header">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="index.html" rel="home">Moschino</a></h1>
+			<h1 class="site-title">
+                <nuxt-link to="/" rel="home">
+                    Moschino
+                </nuxt-link>
+            </h1>
 			<h2 class="site-description">Minimalist Portfolio HTML Template</h2>
 		</div>
 		<nav id="site-navigation" class="main-navigation">
@@ -82,16 +86,11 @@ export default {
                         },
                         {
                             id: 9,
-                            name: 'Blog Article',
-                            url: '/blog-single'
-                        },
-                        {
-                            id: 10,
                             name: 'Shop Item',
                             url: '/shop-single'
                         },
                         {
-                            id: 11,
+                            id: 10,
                             name: 'Portfolio Category',
                             url: '/portfolio-category'
                         }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'moschino',
+    title: 'Moschino | Minimalist Free HTML Portfolio by WowThemes.net',
     htmlAttrs: {
       lang: 'en'
     },
